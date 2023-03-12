d520363eb4d81fef07815344f34b553e7e6f369c
pavaliek <leonismo097@gmail.com>
2023-03-12 09:33:04 -0300

    final commit

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+alert('Olá');
+/*
+OBJETIVO - quando clicarmos no botão temos que
+mostrar a imagem de fundo correspondente
+- passo 1 - dar um jeito de pegar o elemento HTML
+dos botões
+- passo 2 - dar um jeito de identificar o clique
+do usuário no botão
+- passo 3 - desmarcar o'botão selecionado anterior
+- passo 4 - marcar o botão clicado como se
+estivesse selecionada
+- passo 5 - esconder a imagem anterior
+- passo 6 - fazer aparecer a imagem
+correspondente ao botão clicado
+*/
+
+        /*passo 1*/
+        const botoesCarrossel = document.querySelectorAll('.botao');
+        const imagens = document.querySelectorAll('.imagem');
+        /*passo 2*/
+        botoesCarrossel.forEach((botao, indice) => {
+        botao.addEventListener('click', () => {
+
+        console.log(indice);
+           
+        /*passo 3*/
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+        /*passo 4*/
+        botao.classList.add('selecionado');
+        /*passo 5*/
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+        /*passo 6*/
+        imagens[indice].classList.add('ativa');
+        
+    }
+    )
+})
